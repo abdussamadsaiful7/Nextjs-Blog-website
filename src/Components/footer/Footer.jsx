81563013364e63c41988ef7efbfca0eac987c0c3
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-17 03:56:09 +0600

    added footer component

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import style from './page.module.css';
+import Image from 'next/image';
 
 const Footer = () => {
     return (
-        <div>
+        <div className={style.footer}>
             <div>@2023 Art Gallery. All right reserved</div>
-            <div></div>
+            <div className={style.social}>
+
+                <Image src="/facebook.png"  className={style.icon} width={30} height={30} alt='img' />
+                <Image src="/linkedin.png"  className={style.icon} width={30} height={30} alt='img' />
+                <Image src="/pinterest.png" className={style.icon}  width={30} height={30} alt='img' />
+                <Image src="/youtube.png"   className={style.icon}width={30} height={30} alt='img'     />
+                <Image src="/instagram.png" className={style.icon}  width={30} height={30} alt='img' />
+
+            </div>
         </div>
     );
 };
