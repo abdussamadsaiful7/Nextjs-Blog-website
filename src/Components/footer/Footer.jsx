efd15ab8c410253fd7ae61fbffb55762d1ce14c4
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-17 05:09:38 +0600

    added home page

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -9,6 +9,7 @@ const Footer = () => {
             <div className={style.social}>
 
                 <Image src="/facebook.png"  className={style.icon} width={30} height={30} alt='img' />
+                <Image src="/twitter.png"  className={style.icon} width={30} height={30} alt='img' />
                 <Image src="/linkedin.png"  className={style.icon} width={30} height={30} alt='img' />
                 <Image src="/pinterest.png" className={style.icon}  width={30} height={30} alt='img' />
                 <Image src="/youtube.png"   className={style.icon}width={30} height={30} alt='img'     />
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,18 @@
+"use client"
 import Image from 'next/image'
+import style from './page.module.css';
+import hero from '../../public/hero.png';
 
 export default function Home() {
   return (
-    <div>
-        Home page
+    <div className={style.home}>
+      <div className={style.item}>
+        <h1 className={style.title}>Better design <br/> for your digital products</h1>
+        <p className={style.desc}>Turning your Idea into Reality. We bring together the teams from the global tech industry. Good service and standard design is our motto.</p>
+        <button className={style.button}>See Our Works</button>
+      </div>
+      <div className={style.item}></div>
+       <Image src={hero} className={style.bannerImg} alt='img'/>
     </div>
   )
 }
