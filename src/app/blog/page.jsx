e67d0eef73eb983f8987eb735bdbf255f4c08758
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-18 01:57:42 +0600

    added blog contant

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,167 @@
+"use client"
 import React from 'react';
 import style from './page.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Blog = () => {
     return (
-        <div>
-            Blog page
+        <div className={style.mainContainer}>
+            {/* card-1 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img1.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+
+              {/* card-2 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img4.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+
+              {/* card-3 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img5.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+
+              {/* card-4 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img6.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+
+              {/* card-5 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img7.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+              {/* card-6 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img9.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+
+              {/* card-7 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img10.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+              {/* card-8 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img11.webp" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+              {/* card-9 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img12.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
+              {/* card-10 */}
+            <Link href="/blog/textId" className={style.container}>
+                <div className={style.imgContainer}>
+                    <Image 
+                    src="/img13.jpg" 
+                    alt=''
+                    width={600} 
+                    height={400}
+                    className={style.image} />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore culpa perferendis sed asperiores incidunt. Fuga eius et eos dignissimos, atque velit recusandae accusamus repudiandae sequi facilis ratione dicta. Veniam, unde accusamus. Nostrum quibusdam iusto rem voluptas quia neque sequi modi quod similique voluptate reprehenderit nam commodi, suscipit placeat. Distinctio, dicta?</p>
+                </div>
+            </Link>
         </div>
     );
 };
