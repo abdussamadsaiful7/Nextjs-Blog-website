88d6c55fae3eb209f657b66cd1d0d6bee42d08f4
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-17 16:25:13 +0600

    added about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,40 @@
+'use client'
 import React from 'react';
 import style from './page.module.css';
+import Image from 'next/image';
+import Button from '@/Components/Button/Button';
 
 const About = () => {
     return (
-        <div>
-            About page
+        <div className={style.container}>
+            <div className={style.imgContainer}>
+                <Image className={style.img} src="/about4.jpg" fill={true} alt="" />
+            </div>
+            <div className={style.imgText}>
+                <h1 className={style.imgTitle}>Digital Storyteller</h1>
+                <h1 className={style.imgDecs}>Handcrafting award winning digital experiences</h1>
+            </div>
+            <div className={style.textContainer}>
+
+                <div className={style.item}>
+                    <h1 className={style.title}>Who are we?</h1>
+                    <p className={style.desc}>Welcome to our art gallery store! We are a curated space that showcases a diverse collection of artworks from talented artists, ranging from paintings and sculptures to photography and mixed media. Our goal is to inspire and engage art enthusiasts, collectors, and the broader community with captivating and thought-provoking pieces.
+                        <br />  <br />
+                        Whether you are seeking to enrich your personal art collection or simply appreciate creativity at its finest, we aim to provide a delightful experience through our carefully curated exhibits.</p>
+                </div>
+
+                <div className={style.item}>
+                    <h1 className={style.title}>What we do?</h1>
+                    <p className={style.desc}>
+                        We are an art gallery store, passionately curating and exhibiting a diverse range of captivating artworks. Our mission is to promote talented artists, inspire creativity, and provide art enthusiasts with an enriching experience. Discover stunning paintings, sculptures, photography, and more, as we create a haven for art lovers and collectors alike.
+                        <br/>  <br />
+                        - Dynamic design 
+                        <br/>
+                        - Fast and handy mobile service
+                    </p>
+                    <Button url="/contact" text="Contact"/>
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,17 +2,21 @@
 import Image from 'next/image'
 import style from './page.module.css';
 import hero from '../../public/hero.png';
+import Button from '@/Components/Button/Button';
 
 export default function Home() {
   return (
     <div className={style.home}>
       <div className={style.item}>
-        <h1 className={style.title}>Better design <br/> for your digital products</h1>
+        <h1 className={style.title}>
+           Realm
+          Discover a World of Artistic Brilliance</h1>
         <p className={style.desc}>Turning your Idea into Reality. We bring together the teams from the global tech industry. Good service and standard design is our motto.</p>
-        <button className={style.button}>See Our Works</button>
+        {/* <button className={style.container}>See Our Works</button> */}
+        <Button url="/portfolio" text="See Our Works" />
       </div>
       <div className={style.item}></div>
-       <Image src={hero} className={style.bannerImg} alt='img'/>
+      <Image src={hero} className={style.bannerImg} alt='img' />
     </div>
   )
 }
