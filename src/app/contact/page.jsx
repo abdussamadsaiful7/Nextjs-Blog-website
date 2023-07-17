1b31fc8d2e5a1bb5b5bc580c1434090d43d120d1
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-17 19:18:46 +0600

    added contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import style from './page.module.css';
+import Image from 'next/image';
+import Button from '@/Components/Button/Button';
 
 const Contact = () => {
     return (
-        <div className={style.contact}>
-            Contact page
+        <div className={style.container}>
+            <h1 className={style.title}>Let's Keep in Touch</h1>
+            <div className={style.contact}>
+                <div className={style.imgContainer}>
+                    <Image src="/man.png" fill={true} alt="image" 
+                    className={style.image}/>
+                </div>
+                <div className={style.form}>
+                    <input type="text" placeholder='name' className={style.input}/>
+                    <input type="email" placeholder='email' className={style.input}/>
+                    <textarea className={style.textArea} placeholder='message' cols="50" row="50">
+                    </textarea>
+                    <Button url="#" text="Send"/>
+                </div>
+            </div>
         </div>
     );
 };
